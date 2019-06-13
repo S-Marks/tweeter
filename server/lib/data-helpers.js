07e60aab94030f4bff48d2d40d1453d499dd5e5f
S-Marks <marks.cherie.art@gmail.com>
2019-06-13 22:37:40 +0000

    Insert mongo database

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,31 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+// const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+    saveTweet: function (newTweet, callback) {
+      console.log('db: ', db);
+      console.log('collection: ', db.collection('tweets'));
+      db.collection('tweets').insert(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
+    getTweets: function (callback) {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        console.log(tweets);
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
-
+    
   };
 }
